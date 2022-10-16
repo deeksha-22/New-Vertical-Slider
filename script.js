c4c0370ac08e32d43f40e183ab60ea33a91a2075
Lakshya <72151804+0xlsd@users.noreply.github.com>
2022-10-17 01:05:17 +0530

    DRY: reuse direction enum

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ const upButton = document.querySelector(".up-button");
 const downButton = document.querySelector(".down-button");
 const slidesLength = slideRight.querySelectorAll("div").length;
 
+const DIRECTION_ENUM = {
+  UP: "up",
+  DOWN: "down",
+};
 let activeSlideIndex = 0;
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
-upButton.addEventListener("click", () => changeSlide("up"));
-downButton.addEventListener("click", () => changeSlide("down"));
+upButton.addEventListener("click", () => changeSlide(DIRECTION_ENUM.UP));
+downButton.addEventListener("click", () => changeSlide(DIRECTION_ENUM.DOWN));
 
 const changeSlide = (direction) => {
   const sliderHeight = sliderContainer.clientHeight;
-  const DIRECTION_ENUM = {
-    UP: "up",
-    DOWN: "down",
-  };
+  
   if (direction === DIRECTION_ENUM.UP) {
     activeSlideIndex++;
     if (activeSlideIndex > slidesLength - 1) {
